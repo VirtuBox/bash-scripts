b5f98c32633534d625104a1e86ff301260527f8c
VirtuBox <contact@virtubox.net>
2018-04-12 13:44:16 +0200

    Create update-source.sh

---
+++ xmrigCC/update-source.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+release=1.6.0-beta1
+
+# stop xmrigcc
+sudo systemctl stop xmrigcc 
+
+cd /etc/xmrigCC || exit
+
+# get the last release
+git fetch
+git checkout $release
+
+# compile xmrigcc
+cmake . -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7
+make -j "$(nproc)"
+
+# restart xmrigcc
+service xmrigcc start
