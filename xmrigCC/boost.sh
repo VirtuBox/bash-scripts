11350925a58a853c64df9afeb05a0b851c9658eb
VirtuBox <contact@virtubox.net>
2018-04-17 15:38:31 +0200

    Update boost.sh

--- xmrigCC/boost.sh
+++ xmrigCC/boost.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-cd /etc/xmrigCC || exit
+cd /etc || exit
 wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.bz2
 tar xvfj boost_1_67_0.tar.bz2
-cd boost_1_67_0 || exit
+mv boost_1_67_0 boost
+cd boost || exit
 ./bootstrap.sh --with-libraries=system
 ./b2
