d465eb6ec712a62de512d4da8c1f163063db94ff
VirtuBox <contact@virtubox.net>
2018-04-17 15:22:48 +0200

    fix error

--- xmrigCC/update-static.sh
+++ xmrigCC/update-static.sh
@@ -3,7 +3,7 @@
 release=https://github.com/Bendr0id/xmrigCC/releases/download/1.6.0/xmrigCC-1.6.0-with_tls-gcc7-linux-static-miner_only-x64.tar.gz
 
 service xmrigcc stop
-wget {$release} -O xmrigcc.tar.gz
+wget $release -O xmrigcc.tar.gz
 mv config.json c
 tar -xzvf xmrigcc.tar.gz
 rm xmrigcc.tar.gz
