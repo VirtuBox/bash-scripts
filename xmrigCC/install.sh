9f6d5a0350716fffff3cbcd854b859b30d78694d
VirtuBox <contact@virtubox.net>
2018-04-17 18:35:36 +0200

    Update install.sh

--- xmrigCC/install.sh
+++ xmrigCC/install.sh
@@ -20,15 +20,26 @@ sudo add-apt-repository ppa:jonathonf/gcc-7.1 -y
 sudo apt-get update
 sudo apt-get install gcc-7 g++-7  -y
 
-# download xmrigCC
+# install libboost if needed
 
+if [ ! -d /etc/boost ]; then
+cd /etc || exit
+wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.bz2
+tar xvfj boost_1_67_0.tar.bz2 && rm -rf boost_1_67_0.tar.bz2
+mv boost_1_67_0 boost
+cd boost || exit
+./bootstrap.sh --with-libraries=system
+./b2
+fi
+
+# download xmrigCC
 cd /etc || exit
 sudo git clone https://github.com/Bendr0id/xmrigCC.git
 sudo chown -R $USER:$USER xmrigCC
 
 # build xmrigCC
 cd xmrigCC || exit
-cmake . -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7
+cmake . -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7 -DBOOST_ROOT=/etc/boost
 make -j "$(nproc)"
 
 # create xmrigCC systemd service
