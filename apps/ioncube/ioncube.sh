6b6f0280378bc400872bbc50744be36c9981a03b
VirtuBox <contact@virtubox.net>
2018-08-23 11:24:44 +0200

    add ioncube installer

---
+++ apps/ioncube/ioncube.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ ! -z $1 ]; then
+    PHP_VER=$1
+else
+    PHP_INI=$(php -i | grep "Loaded Configuration File" | awk -F "=> " '{print $2}')
+    PHP_VER=$(echo $PHP_INI | awk -F "/" '{print $4}')
+fi
+
+EXTENSION_DIR=$(/usr/bin/php$PHP_VER -i | grep extension_dir | awk -F "=> " '{print $2}')
+
+if [ -f ioncube_loaders_lin_x86-64.tar.gz ]; then
+    rm ioncube_loaders_lin_x86-64.tar.gz
+fi
+if [ -d ioncube ]; then
+    rm -rf ioncube
+fi
+
+wget https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+tar -xvzf ioncube_loaders_lin_x86-64.tar.gz
+cd ioncube || exit 0
+
+cp ioncube_loader_lin_$PHP_VER.so $EXTENSION_DIR/
+
+if [ ! -f /etc/php/$PHP_VER/fpm/conf.d/00-ioncube-loader.ini ]; then
+    echo "zend_extension=ioncube_loader_lin_$PHP_VER.so" >/etc/php/$PHP_VER/fpm/conf.d/00-ioncube-loader.ini
+fi
+if [ ! -f /etc/php/$PHP_VER/cli/conf.d/00-ioncube-loader.ini ]; then
+    echo "zend_extension=ioncube_loader_lin_$PHP_VER.so" >/etc/php/$PHP_VER/cli/conf.d/00-ioncube-loader.ini
+fi
+
+service php$PHP_VER-fpm restart
