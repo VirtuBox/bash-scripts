22952e634f5491dd91cb091bd7971ba88ad9c3e7
VirtuBox <contact@virtubox.net>
2018-08-23 15:56:10 +0200

    Update install.sh

--- cryptocurrency/xmrigCC/install.sh
+++ cryptocurrency/xmrigCC/install.sh
@@ -16,9 +16,19 @@ sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev libs
 
 # install gcc-7
 
-sudo add-apt-repository ppa:jonathonf/gcc-7.1 -y
-sudo apt-get update
-sudo apt-get install gcc-7 g++-7  -y
+if [ ! -f /etc/apt/sources.list.d/jonathonf-ubuntu-gcc-8_1-bionic.list ] && [ ! -f /etc/apt/sources.list.d/jonathonf-ubuntu-gcc-8_1-xenial.list ]; then
+    
+    apt-get install software-properties-common -y
+    add-apt-repository ppa:jonathonf/gcc-8.1 -y
+    apt-get update
+    apt-get install gcc-8 g++-8 -y
+    export CC="/usr/bin/gcc-8"
+    export CXX="/usr/bin/gc++-8"
+fi
+
+#sudo add-apt-repository ppa:jonathonf/gcc-7.1 -y
+#sudo apt-get update
+#sudo apt-get install gcc-7 g++-7  -y
 
 # install libboost if needed
 
@@ -29,7 +39,7 @@ tar xvfj boost_1_67_0.tar.bz2 && rm -rf boost_1_67_0.tar.bz2
 mv boost_1_67_0 boost
 cd boost || exit
 ./bootstrap.sh --with-libraries=system
-./b2
+./b2 --toolset=gcc-8
 fi
 
 # download xmrigCC
@@ -39,7 +49,7 @@ sudo chown -R $USER:$USER xmrigCC
 
 # build xmrigCC
 cd xmrigCC || exit
-cmake . -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7 -DBOOST_ROOT=/etc/boost
+cmake . -DCMAKE_C_COMPILER=gcc-8 -DCMAKE_CXX_COMPILER=g++-8 -DBOOST_ROOT=/etc/boost
 make -j "$(nproc)"
 
 # create xmrigCC systemd service
