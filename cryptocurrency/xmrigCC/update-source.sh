1c65013253b6c4ca961bf6e159af6dfb5500bc1d
VirtuBox <contact@virtubox.net>
2018-08-23 16:08:40 +0200

    Update update-source.sh

--- cryptocurrency/xmrigCC/update-source.sh
+++ cryptocurrency/xmrigCC/update-source.sh
@@ -5,28 +5,33 @@ release=1.7.0-beta1
 # stop xmrigcc
 sudo systemctl stop xmrigcc 
 
+# install gcc-8
 
-
-if [ ! -f /etc/apt/sources.list.d/jonathonf-ubuntu-gcc-7_1-xenial.list ]; then
-                apt-get install software-properties-common -y
-                add-apt-repository ppa:jonathonf/gcc-7.1 -y
-                apt-get update
-                apt-get install gcc-7 g++-7 -y
-                export CC="/usr/bin/gcc-7"
-                export CXX="/usr/bin/gc++-7"
+if [ ! -f /etc/apt/sources.list.d/jonathonf-ubuntu-gcc-8_1-bionic.list ] && [ ! -f /etc/apt/sources.list.d/jonathonf-ubuntu-gcc-8_1-xenial.list ]; then
+    
+    apt-get install software-properties-common -y
+    add-apt-repository ppa:jonathonf/gcc-8.1 -y
+    apt-get update
+    apt-get install gcc-8 g++-8 -y
+    export CC="/usr/bin/gcc-8"
+    export CXX="/usr/bin/gc++-8"
 fi
 
+#sudo add-apt-repository ppa:jonathonf/gcc-7.1 -y
+#sudo apt-get update
+#sudo apt-get install gcc-7 g++-7  -y
 
-                
-if [ -d /etc/boost ]; then
+# install libboost if needed
+
+if [  -d /etc/boost ]; then
+rm -rf /etc/boost
 cd /etc || exit
-rm -rf boost
 wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.bz2
 tar xvfj boost_1_67_0.tar.bz2 && rm -rf boost_1_67_0.tar.bz2
 mv boost_1_67_0 boost
 cd boost || exit
 ./bootstrap.sh --with-libraries=system
-./b2 --toolset=gcc-7
+./b2 --toolset=gcc-8
 fi
 
 cd /etc/xmrigCC || exit
@@ -36,7 +41,7 @@ git fetch
 git checkout $release
 
 # compile xmrigcc
-cmake . -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7 -DBOOST_ROOT=/etc/boost
+cmake . -DCMAKE_C_COMPILER=gcc-8 -DCMAKE_CXX_COMPILER=g++-8 -DBOOST_ROOT=/etc/boost
 make -j "$(nproc)"
 
 # restart xmrigcc
