120f9333441ffa2fb99dc8008de2f6691a105348
VirtuBox <contact@virtubox.net>
2018-11-14 13:37:30 +0100

    Update update-source.sh

--- cryptocurrency/xmrigCC/update-source.sh
+++ cryptocurrency/xmrigCC/update-source.sh
@@ -6,30 +6,50 @@ release=1.8.2
 sudo systemctl stop xmrigcc 
 
 
+##################################
+# Install gcc7 or gcc8 from PPA
+##################################
+# gcc7 for nginx stable on Ubuntu 16.04 LTS
+# gcc8 for nginx mainline on Ubuntu 16.04 LTS & 18.04 LTS
 
-if [ ! -f /etc/apt/sources.list.d/jonathonf-ubuntu-gcc-7_1-xenial.list ]; then
-                apt-get install software-properties-common -y
-                add-apt-repository ppa:jonathonf/gcc-7.1 -y
-                apt-get update
-                apt-get install gcc-7 g++-7 -y
-                export CC="/usr/bin/gcc-7"
-                export CXX="/usr/bin/gc++-7"
+# Checking lsb_release package
+if [ ! -x /usr/bin/lsb_release ]; then
+    sudo apt-get -y install lsb-release | sudo tee -a /tmp/nginx-ee.log 2>&1
 fi
+
+# install gcc-7
+distro_version=$(lsb_release -sc)
+
+
+    if [ "$distro_version" == "bionic" ] && [ ! -f /etc/apt/sources.list.d/jonathonf-ubuntu-gcc-bionic.list ]; then
+        apt-get install software-properties-common -y
+        add-apt-repository -y ppa:jonathonf/gcc
+        elif [ "$distro_version" == "xenial" ] && [ ! -f /etc/apt/sources.list.d/jonathonf-ubuntu-gcc-xenial.list ]; then
+        apt-get install software-properties-common -y
+        add-apt-repository -y ppa:jonathonf/gcc
+    fi
+    apt-get update
+    apt-get full-upgrade
+    apt-get install gcc-7 g++-7 -y
+    export CC="/usr/bin/gcc-7"
+    export CXX="/usr/bin/gc++-7"
+
+
 
 
                 
 if [ ! -d /etc/boost ]; then
-cd /etc || exit
+cd /etc || exit 1
 rm -rf boost
 wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.bz2
-tar xvfj boost_1_67_0.tar.bz2 && rm -rf boost_1_67_0.tar.bz2
+tar xvfj boost_1_67_0.tar.bz2 && rm -f boost_1_67_0.tar.bz2
 mv boost_1_67_0 boost
-cd boost || exit
+cd boost || exit 1
 ./bootstrap.sh --with-libraries=system
 ./b2 --toolset=gcc-7
 fi
 
-cd /etc/xmrigCC || exit
+cd /etc/xmrigCC || exit 1
 
 # get the last release
 git fetch
