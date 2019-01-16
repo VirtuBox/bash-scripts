9fbae25c03d66e46dc4aaca9feca7fb8a09ddcdf
VirtuBox <contact@virtubox.net>
2019-01-16 09:28:00 +0100

    Update update-static.sh

--- cryptocurrency/xmrigCC/update-static.sh
+++ cryptocurrency/xmrigCC/update-static.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-release=https://github.com/Bendr0id/xmrigCC/releases/download/1.8.6/xmrigCC-1.8.6-with_tls-gcc7-linux-static-miner_only-x64.tar.gz
+release=https://github.com/Bendr0id/xmrigCC/releases/download/1.8.10/xmrigCC-1.8.10-with_tls-gcc7-linux-static-miner_only-x64.tar.gz
 
 
 cd /etc/xmrigCC || exit 1
 sudo service xmrigcc stop
-wget -O xmrigcc.tar.gz $release
+wget -O xmrigcc.tar.gz "$release"
 mv config.json c
 /bin/tar -xzf xmrigcc.tar.gz
-rm xmrigcc.tar.gz
-rm config.json
+rm -f xmrigcc.tar.gz
+rm -f config.json
 mv c config.json
 
 sudo wget -O /lib/systemd/system/xmrigcc.service https://raw.githubusercontent.com/VirtuBox/bash-scripts/master/cryptocurrency/xmrigCC/xmrigcc.service
 sudo systemctl daemon-reload
-
 
 sudo service xmrigcc start
 
