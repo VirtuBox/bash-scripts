29c6542eded057c58e2eedd4eb2a7d3028c853af
VirtuBox <contact@virtubox.net>
2019-01-27 21:27:54 +0100

    improve ioncube script

--- apps/ioncube/ioncube.sh
+++ apps/ioncube/ioncube.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
-if [ ! -z "$1" ]; then
-    PHP_VER=$1
+cd "$HOME" || exit 1
+
+if [ "$1" ]; then
+    PHP_VER="$1"
 else
-    PHP_INI=$(php -i | grep "Loaded Configuration File" | awk -F "=> " '{print $2}')
-    PHP_VER=$(echo $PHP_INI | awk -F "/" '{print $4}')
+    PHP_VER=$(/usr/bin/php -i | grep "Loaded Configuration File" | awk -F "=> " '{print $2}' | awk -F "/" '{print $4}')
 fi
 
 EXTENSION_DIR=$(/usr/bin/php${PHP_VER} -i | grep extension_dir | awk -F "=> " '{print $2}')
 
-if [ -f ./ioncube_loaders_lin_x86-64.tar.gz ]; then
-    rm ioncube_loaders_lin_x86-64.tar.gz
-fi
-if [ -d ioncube ]; then
-    rm -rf ioncube
-fi
+rm -f ioncube*.tar.gz
+rm -rf ioncube
 
-wget -q https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
-tar -xvzf ioncube_loaders_lin_x86-64.tar.gz
-rm ioncube_loaders_lin_x86-64.tar.gz
+wget -O ioncube.tar.gz https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+tar -xvzf ioncube.tar.gz
+rm -f ioncube.tar.gz
+
 cd ioncube || exit 1
-
-cp ioncube_loader_lin_$PHP_VER.so $EXTENSION_DIR -f
+cp ioncube_loader_lin_$PHP_VER.so "$EXTENSION_DIR" -f
 
 FPM_CHECK=$(grep "zend_extension=ioncube_loader_lin_${PHP_VER}.so" -r /etc/php/${PHP_VER}/fpm/conf.d)
 CLI_CHECK=$(grep "zend_extension=ioncube_loader_lin_${PHP_VER}.so" -r /etc/php/${PHP_VER}/cli/conf.d)
@@ -33,3 +30,5 @@ if [ -z "$CLI_CHECK" ]; then
 fi
 
 service php${PHP_VER}-fpm restart
+
+rm -rf ioncube
