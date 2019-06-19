8223b8d92814fad0f6c426bfc53fdec6d94f8c2b
VirtuBox <thomas@virtubox.net>
2019-06-19 10:36:53 +0200

    Update phpmyadmin script

--- apps/phpmyadmin/update.sh
+++ apps/phpmyadmin/update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-PHPMYADMIN_VERSION="4.8.5"
+PHPMYADMIN_VERSION="4.9.0.1"
 
 if [ "$(id -u)" != "0" ]; then
 	echo "Error: You must be root to run this script, please use the root user to install the software."
@@ -8,13 +8,13 @@ if [ "$(id -u)" != "0" ]; then
 	exit 1
 fi
 
-
 cd /var/www/22222/htdocs/db/ || exit 1
-wget https://files.phpmyadmin.net/phpMyAdmin/$PHPMYADMIN_VERSION/phpMyAdmin-$PHPMYADMIN_VERSION-all-languages.zip -O phpmyadmin.zip
+wget -O phpmyadmin.zip https://files.phpmyadmin.net/phpMyAdmin/$PHPMYADMIN_VERSION/phpMyAdmin-$PHPMYADMIN_VERSION-all-languages.zip
 
 unzip phpmyadmin.zip && rm phpmyadmin.zip
 mv pma/config.inc.php .
 cp -rf phpMyAdmin-$PHPMYADMIN_VERSION-all-languages/* pma/
 mv config.inc.php pma/
+rm -rf phpMyAdmin-$PHPMYADMIN_VERSION-all-languages
 
 sudo chown -R www-data:www-data pma
